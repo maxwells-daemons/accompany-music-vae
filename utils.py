d73382dcee20319e35e61f9cbcf667c8befc253d
brendan-hollaway <brendan.hollaway@caltech.edu>
2019-05-16 02:47:24 -0700

    Add mostly untested Inference ipynb

--- utils.py
+++ utils.py
@@ -25,3 +25,25 @@ def strip_to_melody(sequence):
     del melody.notes[:]
     melody.notes.extend(melody_notes)
     return melody
+
+
+def remove_melody(sequence):
+    '''
+    Strip a NoteSequence of the melody.
+
+    Parameters
+    ----------
+    sequence : NoteSequence
+        The input sequence.
+
+    Returns
+    -------
+    NoteSequence
+        The input sequence's non-melody as a NoteSequence.
+    '''
+    not_melody = deepcopy(sequence)
+    not_melody_notes = list(filter(lambda note: note.instrument,
+                                   sequence.notes[:]))
+    del not_melody.notes[:]
+    not_melody.notes.extend(not_melody_notes)
+    return not_melody
