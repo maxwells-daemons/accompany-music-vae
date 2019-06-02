6724305da9ed3d285857fc5f424ff1c4c5140c73
Whillikers <aidanswope@gmail.com>
2019-06-02 05:17:29 -0700

    Various changes to model code

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 MODEL_NAME = 'bi_rnn_test'
 MODEL_SAVE_PATH = './models/checkpoints/{}/'.format(MODEL_NAME)
-CHECKPOINT_PATH = MODEL_SAVE_PATH + '{epoch:02d}-{val_acc:.4f}.hdf5'
+CHECKPOINT_PATH = MODEL_SAVE_PATH + '{epoch:02d}-{val_loss:.4f}.hdf5'
 
 TIMESTEPS = 256
 DIM_MELODY = 90
